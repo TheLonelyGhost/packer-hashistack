7af1843b83f5f66080668a03005a390329737385
David Alexander <opensource@thelonelyghost.com>
2020-07-28 22:50:49 -0400

    Installs consul as a CentOS 8.x image on Linode

---
+++ scripts/base.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+dnf update -y
+dnf install -y unzip curl policycoreutils-python-utils perl tar firewalld
+
+if ! firewall-cmd --zone='public' --list-services | grep -e ssh 1>/dev/null; then
+  printf '>>>  Allow SSH communications over WAN\n'
+  firewall-cmd --permanent --zone='public' --add-service='ssh'
+fi
+
+printf '>>>  Setting timezone to UTC\n'
+timedatectl set-timezone UTC && sleep 2
+
+printf '>>>  Disabling root login over SSH\n'
+perl -i -pe's/^(#+)?\s*PermitRootLogin (yes|no)(.*)/PermitRootLogin no\3/g' /etc/ssh/sshd_config
+
+printf '>>>  Disabling password login over SSH\n'
+perl -i -pe's/^(#+)?\s*PasswordAuthentication (yes|no)(.*)/PasswordAuthentication no\3/g' /etc/ssh/sshd_config
+
+if [ "${SSH_PORT:-22}" != '22' ]; then
+  printf '>>>  Changing SSH port\n'
+  perl -i -pe's/^(#+)?\s*Port 22\s*$/Port '"${SSH_PORT}"'\n/g' /etc/ssh/sshd_config
+  semanage port -a -t ssh_port_t -p tcp "${SSH_PORT}"
+  firewall-cmd --permanent --service='ssh' --add-port="${SSH_PORT}/tcp"
+  firewall-cmd --reload
+fi
+
+printf '>>>  Deduping SSH config settings (where lines are exactly the same)\n'
+perl -i -lne '$seen{$_}++ and next or print;' /etc/ssh/sshd_config
+
+# Setup another SSH user for machine access, since we've disabled root and password auth
+if [ -n "${ENTRY_USER:-}" -a -n "${ENTRY_USER_PUBKEY:-}" ]; then
+  printf '>>>  Creating automation-access user account %s\n' "${ENTRY_USER}"
+  useradd "${ENTRY_USER}"
+  printf '%s:%s\n' "${ENTRY_USER}" 'U6aMy0wojraho' | chpasswd -e 1>/dev/null
+  printf '%s ALL = (ALL) NOPASSWD: ALL\n' "${ENTRY_USER}" >> /etc/sudoers
+  mkdir -p /home/"${ENTRY_USER}"/.ssh
+  printf '%s\n' "${ENTRY_USER_PUBKEY}" >> /home/"${ENTRY_USER}"/.ssh/authorized_keys
+  chown -R "${ENTRY_USER}:${ENTRY_USER}" /home/"${ENTRY_USER}"
+  chmod 700 /home/"${ENTRY_USER}"/.ssh
+  chmod 600 /home/"${ENTRY_USER}"/.ssh/authorized_keys
+fi
+
+# LINODE: Add private IPv4 range to trusted network zone
+firewall-cmd --permanent --zone='trusted' --add-source='192.168.128.0/17'
---
+++ scripts/install-consul.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+: ${CONSUL_VERSION:=1.8.0}
+
+printf '>>>  Downloading Consul v%s...\n' "$CONSUL_VERSION"
+curl -SsLo ./consul_SHA256SUMS https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_SHA256SUMS
+curl -SsLo ./consul.zip https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip
+
+if [ -e ./consul_SHA256SUMS ]; then
+  printf '>>>  Verifying Consul download...\n'
+  awk '/_linux_amd64/ { print $1 " consul.zip" }' ./consul_SHA256SUMS | sha256sum --check -
+  rm ./consul_SHA256SUMS
+fi
+
+printf '>>>  Extracting Consul from archive\n'
+unzip ./consul.zip
+rm ./consul.zip
+
+printf '>>>  Installing Consul binary to /usr/local/bin\n'
+mkdir -p /usr/local/bin
+chmod +x ./consul
+chown root:root ./consul
+mv ./consul /usr/local/bin/consul
+
+/usr/local/bin/consul -autocomplete-install
+complete -C /usr/local/bin/consul consul
+
+printf '>>>  Creating Consul system user\n'
+useradd --system --home /etc/consul.d --shell /bin/false consul
+
+printf '>>>  Creating Consul data directory\n'
+mkdir -p /etc/consul.d
+chmod 600 -R /etc/consul.d
+chmod 700 /etc/consul.d
+chown -R consul:consul /etc/consul.d
+
+# Consul has a VXLAN solution built-in, but HashiCorp recommends using Envoy instead
+printf '>>>  Installing Envoy\n'
+pushd /etc/yum.repos.d/ 1>/dev/null 2>&1
+curl -SLO https://getenvoy.io/linux/centos/tetrate-getenvoy.repo
+dnf makecache -y
+popd 1>/dev/null 2>&1
+dnf install -y getenvoy-envoy
+
+# Make Consul DNS lookups work by using dnsmasq
+dnf -y install dnsmasq
+if [ -e /etc/systemd/resolved.conf ]; then
+  printf '>>>  Workaround: Disabling systemd-resolved as a DNS resolver\n'
+  perl -i -pe's/^(#+)?\s*DNSStubListener=.+$/DNSStubListener=no/g' /etc/systemd/resolved.conf
+  systemctl restart systemd-resolved.service
+fi
+
+systemctl enable dnsmasq
+
+if command -v firewall-cmd 1>/dev/null 2>&1; then
+  # We're registering these as Firewalld services so we can choose to come back
+  # later, when configuring Consul, to modify the ports associated with the each
+  # service. Until then we can apply sane rules for who is allowed to communicate
+  # to what service by assigning these to zones.
+  for service in 'consul-dns' 'consul-grpc' 'consul-http' 'consul-https' 'consul-serf-lan' 'consul-serf-wan' 'consul-server' 'consul-sidecar' 'consul-expose'; do
+    if [ -e "/tmp/firewalld/${service}.xml" ]; then
+      printf '>>>  Installing firewall definition for %s\n' "$service"
+      firewall-cmd --permanent --new-service-from-file="/tmp/firewalld/${service}.xml" --name="${service}"
+    fi
+  done
+
+  firewall-cmd --permanent --zone='public' --add-service='consul-expose'
+  firewall-cmd --permanent --zone='public' --add-service='consul-grpc'
+  firewall-cmd --permanent --zone='public' --add-service='consul-server'
+  firewall-cmd --permanent --zone='public' --add-service='consul-serf-wan'
+  firewall-cmd --permanent --zone='trusted' --add-service='consul-dns'
+  firewall-cmd --permanent --zone='trusted' --add-service='consul-expose'
+  firewall-cmd --permanent --zone='trusted' --add-service='consul-grpc'
+  firewall-cmd --permanent --zone='trusted' --add-service='consul-http'
+  firewall-cmd --permanent --zone='trusted' --add-service='consul-https'
+  firewall-cmd --permanent --zone='trusted' --add-service='consul-serf-lan'
+  firewall-cmd --permanent --zone='trusted' --add-service='consul-serf-wan'
+  firewall-cmd --permanent --zone='trusted' --add-service='consul-server'
+  firewall-cmd --permanent --zone='trusted' --add-service='consul-sidecar'
+fi
+
+systemctl daemon-reload
+systemctl enable consul
