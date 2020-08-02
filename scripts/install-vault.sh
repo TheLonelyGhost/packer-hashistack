ebddb7e794c6f33142fe96fd3738597937555507
David Alexander <opensource@thelonelyghost.com>
2020-08-02 19:00:34 -0400

    Installs Vault
    We defer enabling Vault agent or Vault server to a decision made when
    provisioning the node. To the end-user it will appear the same: some
    service available via HTTP on port 8200 that appears to be Vault. The
    implementation of it varies drastically, though.
    
    If this is a Vault server, we want to allow any (trusted) server to
    communicate against 8200. If it's a Vault agent, we want only localhost
    traffic to communicate. This will need to be established at provisioning
    time so there's no confusion.

---
+++ scripts/install-vault.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+: ${VAULT_VERSION:=1.5.0}
+
+printf '>>>  Downloading Vault v%s...\n' "$VAULT_VERSION"
+curl -SsLo ./vault_SHA256SUMS https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_SHA256SUMS
+curl -SsLo ./vault.zip https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip
+
+if [ -e ./vault_SHA256SUMS ]; then
+  printf '>>>  Verifying Vault download...\n'
+  awk '/_linux_amd64/ { print $1 " vault.zip" }' ./vault_SHA256SUMS | sha256sum --check -
+  rm ./vault_SHA256SUMS
+fi
+
+printf '>>>  Extracting Vault from archive\n'
+unzip ./vault.zip
+rm ./vault.zip
+
+printf '>>>  Installing Vault binary to /usr/local/bin\n'
+mkdir -p /usr/local/bin
+chmod +x ./vault
+chown root:root ./vault
+mv ./vault /usr/local/bin/vault
+setcap cap_ipc_lock=+ep /usr/local/bin/vault
+
+/usr/local/bin/vault -autocomplete-install
+complete -C /usr/local/bin/vault vault
+
+printf '>>>  Creating Vault system user\n'
+useradd --system --home /etc/vault.d --shell /bin/false vault
+
+printf '>>>  Creating Vault data directory\n'
+mkdir -p /etc/vault.d
+chown -R vault:vault /etc/vault.d
+
+
+if command -v firewall-cmd 1>/dev/null 2>&1; then
+  for service in 'vault-api' 'vault-cluster'; do
+    if [ -e "/tmp/firewalld/${service}.xml" ]; then
+      printf '>>>  Installing firewall definition for "%s" service\n' "$service"
+      firewall-cmd --permanent --new-service-from-file="/tmp/firewalld/${service}.xml" --name="${service}"
+    fi
+  done
+
+  firewall-cmd --permanent --zone='trusted' --add-service='vault-api'
+  firewall-cmd --permanent --zone='trusted' --add-service='vault-cluster'
+fi
