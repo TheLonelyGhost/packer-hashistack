da425aad7430ca02822e508c8014bd051afd09d1
David Alexander <opensource@thelonelyghost.com>
2020-09-15 16:04:15 -0400

    Adds placeholder in case we want UUID node names in Raft

--- post-hoc/cluster-join-digitalocean.sh
+++ post-hoc/cluster-join-digitalocean.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+NODE_ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+
 : ${CONSUL_TAG:-consul-server}
 : ${NOMAD_TAG:-nomad-server}
 : ${REGION:-nyc1}
 
 cat >/etc/consul.d/join.hcl <<EOH
 retry_join = ["provider=digitalocean tag_name=${TAG} region=${REGION} api_token=${API_TOKEN}"]
+EOH
+
+cat >/etc/consul.d/node.hcl <<EOH
+# This defaults to the system hostname
+#node_name = "${NODE_ID}"
+EOH
+
+cat >/etc/nomad.d/node.hcl <<EOH
+# This defaults to the system hostname
+#name = "${NODE_ID}"
 EOH
 
 cat >/etc/nomad.d/join.hcl <<EOH
--- post-hoc/cluster-join-linode.sh
+++ post-hoc/cluster-join-linode.sh
@@ -11,6 +11,16 @@ cat >/etc/consul.d/join.hcl <<EOH
 retry_join = ["provider=linode tag_name=${CONSUL_TAG} region=${REGION} api_token=${API_TOKEN}"]
 EOH
 
+cat >/etc/consul.d/node.hcl <<EOH
+# This defaults to the system hostname
+#node_name = "${NODE_ID}"
+EOH
+
+cat >/etc/nomad.d/node.hcl <<EOH
+# This defaults to the system hostname
+#name = "${NODE_ID}"
+EOH
+
 cat >/etc/nomad.d/join.hcl <<EOH
 client {
   server_join {
